0f22ce23b257785601ac21c6d51a76f3f5e4d1c3
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-12-08 00:11:38 -0800

    Push

--- scrapping.py
+++ scrapping.py
@@ -8,13 +8,13 @@ client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
 tweets = []
 
 for response in tweepy.Paginator(client.search_recent_tweets,
-                                 query = 'Shein -is:retweet lang:en', # change brand name
+                                 query = '(Shein OR shein OR SHEIN) -is:retweet lang:en', # change brand name
                                  user_fields = ['username', 'public_metrics', 'description', 'location'],
                                  tweet_fields = ['created_at', 'geo', 'public_metrics', 'text'],
                                  expansions = 'author_id',
                                  start_time = '2021-12-01T09:00:20Z',
                                  end_time = '2021-12-07T00:00:00Z',
-                                 max_results=100).flatten(limit=1200):
+                                 max_results=100).flatten(limit=1):
     time.sleep(1)
     tweets.append(response)
 
@@ -23,31 +23,15 @@ user_dict = {}
 
 # Loop through each response object
 for response in tweets:
-    # Take all of the users, and put them into a dictionary of dictionaries with the info we want to keep
-    for user in response.includes['users']:
-        user_dict[user.id] = {'username': user.username,
-                              'followers': user.public_metrics['followers_count'],
-                              'tweets': user.public_metrics['tweet_count'],
-                              'description': user.description,
-                              'location': user.location
-                             }
-    for tweet in response.data:
-        # For each tweet, find the author's information
-        author_info = user_dict[tweet.author_id]
-        # Put all of the information we want to keep in a single dictionary for each tweet
-        results.append({'author_id': tweet.author_id,
-                       'username': author_info['username'],
-                       'author_followers': author_info['followers'],
-                       'author_tweets': author_info['tweets'],
-                       'author_description': author_info['description'],
-                       'author_location': author_info['location'],
-                       'text': tweet.text,
-                       'created_at': tweet.created_at,
-                       'retweets': tweet.public_metrics['retweet_count'],
-                       'replies': tweet.public_metrics['reply_count'],
-                       'likes': tweet.public_metrics['like_count'],
-                       'quote_count': tweet.public_metrics['quote_count']
-                      })
+    # Put all of the information we want to keep in a single dictionary for each tweet
+    results.append({'author_id': response.author_id,
+                    'text': response.text,
+                    'created_at': response.created_at,
+                    'retweets': response.public_metrics['retweet_count'],
+                    'replies': response.public_metrics['reply_count'],
+                    'likes': response.public_metrics['like_count'],
+                    'quote_count': response.public_metrics['quote_count']
+                    })
 
 # Change this list of dictionaries into a dataframe
 df = pd.DataFrame(results)
